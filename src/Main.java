782794a41f24e2a1a914af1cdf2726d35eea6495
ЛЁША <kovnatali@mail.ru>
2023-02-09 00:26:37 +0700

    домашка объекты и классы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author pushkin = new Author( "Александр", " Пушкин ");
+        Author tolstoy = new Author( " Алексей ", " Толстой ");
+        Book dubrovskii = new Book(" Дубровский", 1833, pushkin);
+        Book aelita = new Book( " Аэлита", 1923, tolstoy);
+
+        print(dubrovskii);
+        print(aelita);
+        dubrovskii.printInfo();
+        aelita.printInfo();
+        dubrovskii.setPublishingYear(1865);
+        dubrovskii.printInfo();
     }
+    private static void print(Book book) {
+        System.out.println("Название книги:" + book.getTitle() + " год издания :" + book.getPublishingYear() +
+                " автор : " + book.getAuthor().getName() + " " + book.getAuthor().getSurname());
+
+    }
+
 }
